06d961205c09cec60ecb5ef1c2317b10ce18f35b
chengbotao <t221007s@163.com>
2020-08-30 14:20:47 +0800

    build: add commit message rules

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: Chengbotao
+ * @Date: 2020-08-23 00:08:07
+ * @LastEditors: Chengbotao
+ * @LastEditTime: 2020-08-23 00:08:17
+ * @FilePath: \vuex-persisted-states\.prettierrc.js
+ */
+module.exports = {
+  // printWidth: 100, //长度超过100断行
+  singleQuote: true,//使用单引号
+};
